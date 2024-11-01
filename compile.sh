3753213dc0a3c262db48419b046f429df1db5b36
beddinao <bilaleddinaoui@gmail.com>
2024-11-01 14:38:38 +0100

    init

---
+++ compile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#cd lib/MLX42/
+
+#cmake -B build 
+#cmake --build build -j4
+
+#cd - && cd lib/bass 
+
+#make 64bit
+
+#cd -
+
+gcc main.c -framework Cocoa -framework IOKit ./MLX42/build/libmlx42.a \
+	-Iinclude -lglfw -L /Users/beddinao/.brew/opt/glfw/lib -fsanitize=address
+
